ccd21b9d328d971a5ad90938fc29159807ad141b
Michael Rauh <michaelrauh@ymail.com>
2013-12-29 23:25:51 -0500

    52 week low calculation added
    Lows are found by comparing against min of the last 52 weeks in sliding
    window.

--- stats.py
+++ stats.py
@@ -1,13 +1,25 @@
 import pickle
+import math
 pointMap = pickle.load(open("points.p","rb"))
 dates = pickle.load(open("dates.p","rb"))
 
-#Here looking just at daily lows
-low = 100000000
-i=0
-yearLows = []
+#make list of lows
+lows = []
+for date in dates:
+    lows.append(pointMap[date][3])
 
-#Find each 52 week low and add to yearLows. Map is not in order so iterate over dates
+low = 100000000
+yearLows = []
+fiftytwoweeks= 5 * 52
+
+#Find each 52 week low and add to yearLows. Map is not in order so iterate
+#over dates
+
+for i in range(fiftytwoweeks,len(dates)): #for dates after first buffer year
+    if min(lows[i - fiftytwoweeks:i-1]) > lows[i]: # if lowest in last 52 weeks is greater than current
+        yearLows.append(dates[i]) #then this is a 52 week low (save the date)
+        
+        
 
 #For each 52 week low, we want to know:
 #1: number of 52 week lows in last 10 days
