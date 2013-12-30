6ee86937164afd94a4b48d0d7cdfe4e3797e94fb
Michael Rauh <michaelrauh@ymail.com>
2013-12-29 20:39:09 -0500

    Converted strings to float values, search started

--- parse.py
+++ parse.py
@@ -10,8 +10,8 @@ points = data.replace('\n',',').split(',') # here is a list with each thing
 
 #create map from date to point data. In practice, it may be useful to map open price or low to point data as we're focusing on 52 week lows
 pointMap = {}
-for i in range (0,len(points)-7,7):
-    pointMap[points[i]] = (points[i+1],points[i+2],points[i+3],points[i+4],points[i+5],points[i+6])
+for i in range (7,len(points)-7,7):
+    pointMap[points[i]] = (float(points[i+1]),float(points[i+2]),float(points[i+3]),float(points[i+4]),float(points[i+5]),float(points[i+6]))
 
 #dump data for later usage
 
--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
 import pickle
-
 pointMap = pickle.load(open("points.p","rb"))
+
+#Here looking just at daily lows
+low = 100000000
+
+#So far, a global low. 52 week low is a little tougher
+for point in pointMap:
+	if (pointMap[point][3] < low):
+		low = pointMap[point][3]
