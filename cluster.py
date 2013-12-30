4cdb3c26f55bf7e0909534c47f7d106d20419e17
Michael Rauh <michaelrauh@ymail.com>
2013-12-30 01:01:53 -0500

    Points organized by year.
    Now that points are organized by year, it is possible to create a set of
    centroids for each year. Bear in mind that there can be any number of
    centroids, based upon the number of categories. For the first run, it
    may be wise to use only two categories.

--- cluster.py
+++ cluster.py
@@ -2,9 +2,15 @@ import pickle
 points = pickle.load(open("coordinates.p","rb"))
 
 #Get centroids for each year
+yearPoints={}
+years = []
 
-#First we have to separate points by year
-
+#First we have to separate points by year. This maps years to lists of tuples containing dates and data.
+for point in points:
+	if not point[0:4] in list(yearPoints):
+		yearPoints[point[0:4]] = []
+		years.append(point[0:4])
+	yearPoints[point[0:4]].append((point,points[point]))
 #find centroid trend and predict next year's centroids
 
 #pickle centroids out
