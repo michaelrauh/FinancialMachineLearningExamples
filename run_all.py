f049e6cfdc95df5a139eca942a8f68e4e3f28e17
Michael Rauh <michaelrauh@ymail.com>
2014-02-05 19:01:40 -0500

    created python file to call other files in order

---
+++ run_all.py
@@ -0,0 +1,13 @@
+print "parse"
+execfile('parse.py')
+del(stocks)
+print "stats"
+execfile('stats.py')
+del(years)
+del(all_points)
+print "cluster"
+execfile('cluster.py')
+del(next_good)
+del(next_bad)
+print "run"
+execfile('run.py')
