7f52d5cb613a11e2a6581fafbc8baf039a828c87
Michael Rauh <michaelrauh@ymail.com>
2014-05-12 13:52:55 -0400

    cleaned up Java stuff

--- stats.py
+++ stats.py
@@ -8,22 +8,23 @@ all_points = []
 for stock in stocks:
     all_points.append(find_coordinates(stock))
 
-##for stock in all_points:
-##    normalize(stock)
-##
-### Produce map from years to buy/sell to list of data.
-### Eg. years[2008][True][0][0] => Open of first good stock in 2008
-##years = {}
-##for stock in all_points:
-##    for date in stock:
-##        raw_stock = stocks[all_points.index(stock)]
-##        good = good_buy(date, raw_stock)
-##        year = int(date[0:4])
-##        if not year in years:
-##            years[year] = {}
-##            years[year][True] = []
-##            years[year][False] = []
-##        years[year][good].append(stock[date])
-##
-##pickle.dump(years,open("pickles/years.p","wb"))
-##pickle.dump(all_points,open("pickles/all_points.p","wb"))
+for stock in all_points:
+    normalize(stock)
+
+# Produce map from years to buy/sell to list of data.
+# Eg. years[2008][True][0][0] => Open of first good stock in 2008
+years = {}
+for stock in all_points:
+    for date in stock:
+        raw_stock = stocks[all_points.index(stock)]
+        good = good_buy(date, raw_stock)
+        year = int(date[0:4])
+        if not year in years:
+            years[year] = {}
+            years[year][True] = []
+            years[year][False] = []
+        years[year][good].append(stock[date])
+
+pickle.dump(years,open("pickles/years.p","wb"))
+pickle.dump(all_points,open("pickles/all_points.p","wb"))
+
