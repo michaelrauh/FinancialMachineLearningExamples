86c03f066c0d586a3123adabbad2995f4f5596c1
Michael Rauh <michaelrauh@ymail.com>
2014-06-19 16:38:03 -0400

    hacked brute forcer together. Should take 3 days

--- cluster.py
+++ cluster.py
@@ -37,16 +37,34 @@ for year in years:
                         for z in range (0,len(flipped)):
                                 avgs[good][year][z] = avg(all_flipped[z])
 
-# Find centroid trend and predict next year's centroids
-next_good = [0 for i in range(len(flipped))]
-next_bad = [0 for i in range(len(flipped))]
+
+
+#this stuff drives the rest and must be brute forced
 deg_good = [1 for i in range(len(flipped))]
 deg_bad = [1 for i in range(len(flipped))]
-back = 0 #max 47 for this data set. 47 looks at only 1 year
+back_good = [0 for i in range(len(flipped))] #max 47 for this data set. 47 looks at only 1 year
+back_bad = [0 for i in range(len(flipped))]
 
-for i in range(0, len(flipped)):
-        next_good[i] = extrapolate(avgs[True], i, deg_good[i], back)
-        next_bad[i] = extrapolate(avgs[False], i, deg_bad[i], back)
-
-pickle.dump(next_good,open("pickles/next_good.p","wb"))
-pickle.dump(next_bad,open("pickles/next_bad.p","wb"))
+total = 0
+for thing in [deg_bad,deg_good]:
+        for a in range(1,6):
+                for b in range(9):
+                        thing[b] = a
+                        for guy in [back_good,back_bad]:
+                                for c in [0,10,20,30,40,47]:
+                                        for d in range(9):
+                                                print total
+                                                total += 1
+                                                """guy[d] = c
+                                                next_good = [0 for i in range(len(flipped))]
+                                                next_bad = [0 for i in range(len(flipped))]
+                                                print 'back_good:', back_good
+                                                print 'back_bad:', back_bad
+                                                print 'deg_good:',deg_good
+                                                print 'deg_bad:', deg_bad
+                                                for i in range(0, len(flipped)):
+                                                        next_good[i] = extrapolate(avgs[True], i, deg_good[i], back_good[i])
+                                                        next_bad[i] = extrapolate(avgs[False], i, deg_bad[i], back_bad[i])
+                                                pickle.dump(next_good,open("pickles/next_good.p","wb"))
+                                                pickle.dump(next_bad,open("pickles/next_bad.p","wb"))
+                                                execfile('run.py')"""
