4e937517033d7b5483110091f9ffe1787ddeb9d5
Michael Rauh <michaelrauh@ymail.com>
2014-06-29 15:45:26 -0400

    finding nearest and showing behavoir

--- hack4.py
+++ hack4.py
@@ -1,12 +1,19 @@
 """Calculate average return by number of previous lows"""
 import cPickle as pickle
 import urllib2
+import sys
 
 opn = 0
 high = 1
 low = 2
 close = 3
 volume = 4
+
+def find_closest(l,x):
+    diffs = []
+    for item in l:
+        diffs.append(abs(item-x))
+    return l[diffs.index(min(diffs))]
 
 def slope_max(dates,date, maximum,point_map):
     """Find slope from maximum to date"""
@@ -27,9 +34,9 @@ def slope_min(dates,date, minimum,point_map):
             return 0
 
 def num_fifty_two(date,yearLows,dates):
-    """Find number of 52 week lows in 10 day period before date"""
+    """Find number of 52 week lows in a month long period before date"""
     count = 0
-    last_ten = dates[dates.index(date)-10:dates.index(date)]
+    last_ten = dates[dates.index(date)-20:dates.index(date)]
     for day in last_ten:
         count += yearLows.count(day)
     return count
@@ -48,6 +55,7 @@ def find_min(maximum,dates,point_map):
         date_index -= 1
     return dates[date_index]
 
+
 def Parse (data):
     """Create map from CSV"""
     points = data.replace('\n',',').split(',') # split csv
@@ -59,18 +67,27 @@ def Parse (data):
     lows=points[3::6]
     closes=points[4::6]
     volumes=points[5::6]
+    banned = []
     
+    for thing in [dates,opens,highs,lows,closes,volumes]:
+        for i in range(len(thing)):
+            if thing[i] == '-':
+                if dates[i] not in banned:
+                    banned.append(dates[i])
+    for bad in banned:
+        dates.remove(bad)
     #create map from date to point data.
     point_map = {}
     for i in range(len(dates)):
-        #print opens[i],highs[i],lows[i],closes[i],volumes[i]
         try:
-            point_map[dates[i]] = (float(opens[i]),float(highs[i]),float(lows[i]),float(closes[i]),float(volumes[i])) 
+            if dates[i] not in banned:
+                point_map[dates[i]] = (float(opens[i]),float(highs[i]),float(lows[i]),float(closes[i]),float(volumes[i])) 
         except:
             point_map[dates[i]] = (float(opens[i-1]),float(highs[i-1]),float(lows[i-1]),float(closes[i-1]),float(volumes[i-1]))
     dates.reverse() #Dates earliest to latest
     stock = (dates,point_map)
     return stock
+
 
 def avg(points):
         """Find average of a list of points"""
@@ -81,7 +98,7 @@ def avg(points):
         except TypeError:
                 return points
 
-def find_coordinates(stock):
+def find_coordinates(stock,verbose = True):
     """Run technical analysis on a given stock"""
     # Make list of lows
     lows = []
@@ -103,13 +120,15 @@ def find_coordinates(stock):
             
 
     coordinates = {}
-    print len(yearLows), 'lows found: '
+    if verbose:
+        print len(yearLows), 'lows found: '
     for date in yearLows:
-        print date
+        if verbose:
+            print date
         maximum = find_max(date,dates,point_map)
         minimum = find_min(maximum,dates,point_map)
         coordinates[date] = []
-        # Number of 52 week lows in 10 day period #
+        # Number of 52 week lows in last month period #
         coordinates[date].append(num_fifty_two(date, yearLows, dates))
 
         # Slope from local max #
@@ -140,7 +159,6 @@ def find_coordinates(stock):
 def future_value(date, stock):
     """Return true if the stock is a good buy"""
     month = 20
-    opn = 0
     interest = 1.08
     dates = stock[0]
     stock = stock[1]
@@ -148,19 +166,23 @@ def future_value(date, stock):
             later = dates [dates.index(date) + month]
     except IndexError:
             later = dates[-1]
-    value = ((stock[later][opn]) - stock[date][opn])/stock[later][opn]
+    value = ((stock[later][close]) - stock[date][close])/stock[later][close]
     return value
+
 
 symbol = raw_input('enter symbol: ')
 start = str(int(raw_input('enter training start year: ')) - 1) # go back one extra year for low detection. This extra year isn't in results.
 end = raw_input('enter training end year: ')
 
+#start = '2000'
+#end = '2014'
+#for symbol in ['bbby','coh','cag','dri','disca','ebay','jec','l','pvh','spls','ibm','wmt','ca','c','rost','urbn']:
 url = 'http://www.google.com/finance/historical?q=' + symbol + '&histperiod=daily&startdate=Jan+1%2C+'+ start + '&enddate=Dec+31%2C+' + end + '&output=csv'
-
+print symbol, str(int(start) + 1), end
 f = urllib2.urlopen(url).read()
 stock = Parse(f)
 
-all_points = find_coordinates(stock)
+all_points = find_coordinates(stock,True)
 low_count = [[] for i in range(20)]
 interest = .08
 
@@ -197,4 +219,61 @@ for count in low_count:
         i+=1
     except:
         pass
+
+start = '2013'
+end = '2014'
+url = 'http://www.google.com/finance/historical?q=' + symbol + '&histperiod=daily&startdate=Jan+1%2C+'+ start + '&enddate=Dec+31%2C+' + end + '&output=csv'
+f = urllib2.urlopen(url).read()
+focus = Parse(f)
+tech = find_coordinates(focus,False)
+try:
+    temp = tech.keys()
+    for i in range(len(temp)):
+        temp[i] = temp[i].split('-')
+    months_numbers = ['00','01','02','03','04','05','06','07','08','09','10','11']
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    x = []
+    for date in temp:
+        if len(date[0]) == 1:
+            date[0] = '0' + date[0]
+        x.append(date[2] + months_numbers[months.index(date[1])] + date[0])
+    x.sort()
+    top = x[-1]
+    yr = top[:2]
+    mn = top[2:4]
+    day = top[4:]
+    if day[0] == '0':
+        day = day[1]
+    recent = day + '-' + months[int(mn)] + '-' + yr
+    print 'Most recent low:',recent
+    print 'Indicators:',tech[recent]
+except IndexError:
+    print 'No recent lows found. Should have bought it a long time ago.'
+    sys.exit()
+
+#set up data for finding closest
+dates = all_points.keys()
+stocks = all_points
+query = tech[recent]
+indicators = [{} for i in range(9)]
+for date in dates:
+    for i in range(9):
+        indicators[i][stocks[date][i]] = date
         
+#get rid of the actual query date
+for i in range(1,9):
+    del(indicators[i][query[i]])
+
+closests = []
+print 'indicator','date','recent','closest','% difference','projected value'
+for i in range(1,9):
+    l = indicators[i].keys()
+    x = query[i]
+    closest = find_closest(l,x)
+    closests.append(closest)
+    date = indicators[i][closest]
+    print i,date , round(x,3), str(round(closest,3)).rjust(10), str(round((abs(closest - x)/float(x)),3) * 100).rjust(10), str(round(future_value(date,stock),3)).rjust(10)
+
+
+
+    
