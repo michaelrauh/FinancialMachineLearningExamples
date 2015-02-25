5c8d55b3718c82dbd78c5c33fb62ddbc52155437
michaelrauh <michaelrauh@ymail.com>
2015-02-24 19:31:56 -0500

    wrote scraper to get raw stock data

---
+++ scraper.py
@@ -0,0 +1,43 @@
+import urllib.request
+import pickle
+
+class Stock:
+    pass
+
+def scrape(symbol, ipo):
+        """Get Data from Google Finance"""
+        if ipo == 'n/a':
+            ipo = '1972' #oldest stock in the table
+        
+        try:
+            PRESENT = '2014'
+            url = 'http://www.google.com/finance/historical?q=' + symbol + \
+                  '&histperiod=daily&startdate=Jan+1%2C+'+ ipo + \
+                  '&enddate=Dec+31%2C+' + PRESENT + '&output=csv'
+            f = urllib.request.urlopen(url).read().decode("utf-8")
+            points = f.replace('\n',',').split(',')
+        except:
+            points = []
+        return points
+
+
+stocks = {}
+for filename in ('nyse.csv','nasdaq.csv'):
+    f = open(filename).read()
+    f = f.split('\n')
+    f.pop(0)
+    for row in f:
+        x = row.split('"')
+        stock = Stock()
+        stock.symbol = x[1]
+        stock.cap = x[7]
+        stock.ipo = x[9]
+        stock.sector = x[11]
+        stock.industry = x[13]
+        stock.data = scrape(stock.symbol,stock.ipo)
+        stocks[stock.symbol] = stock
+        print(len(stocks), stock.symbol, len(stock.data), stock.ipo)
+
+out = open('raw_stocks.txt','wb')
+pickle.dump(stocks,out)
+out.close()
