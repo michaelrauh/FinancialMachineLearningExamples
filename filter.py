041ce8054db6e80e52b2a8facb0574fd5e658574
michaelrauh <michaelrauh@ymail.com>
2015-02-27 16:21:27 -0500

    first draft of top down design for filter

--- filter.py
+++ filter.py
@@ -3,35 +3,53 @@ import pickle
 f = open('raw_stocks.txt','rb')
 stocks = pickle.load(f)
 
-criteria = get_user_input()
+all_clusters = []
+
+def find_highs(stock):
+    """find all 52 week highs for a stock"""
+
+def find_groups(highs, threshold):
+    """given a stock, find groups that agglomerate at a given threshold"""
 
 class Cluster:
     def __init__(self, group, stock):
-        self.dates = group
-        self.beginning = group[0]
-        self.end = group [-1]
-        self.length = elapsed_time(beginning,end)
+        """make a cluster with default data"""
+        self.beginning = find_earliest(group)
+        self.end = find_latest(group)
+        self.length = elapsed_time(beginning, end)
         self.size = len(group)
-        self.price_map = make_price_map(group,stock)
-        self.delta_plus = find_delta_plus(group, stock)
-        self.delta_minus = find_delta_minus(group, stock)
-        self.high_buckets = enumerate_highs(group)
-    def merge_in(self,cluster):
-        self.dates += cluster.dates
-        self.beginning = find_earlier(self.beginning, cluster.beginning)
-        self.end = find_later (self.end, cluster.end)
-        self.length = elapsed_time(beginning,end)
+        self.delta_plus = find_delta_plus(group, stock, self.beginning)
+        self.delta_minus = find_delta_minus(group, stock, self.beginning)
+        self.high_buckets = [[1] for i in range (size)]
+        self.symbols = [stock.symbol]
+        self.cap_range_min = stock.cap
+        self.cap_range_max = stock.cap
+        self.sectors = [stock.sector]
+        self.industries = [stock.industry]
+    def merge_in(self, cluster):
+        """make supercluster by merging clusters"""
+        self.beginning = find_earliest([self.beginning, cluster.beginning])
+        self.end = find_latest ([self.end, cluster.end])
+        self.length = elapsed_time(self.beginning, self.end)
         self.size += cluster.size
-        self.price_map # merge maps
         self.delta_plus = max(self.delta_plus, cluster.delta_plus)
-        self.delta_minus = min(self.delta_plus, cluster.delta_plus)
+        self.delta_minus = min(self.delta_minus, cluster.delta_minus)
         self.high_buckets = merge_high_buckets(self.high_buckets, cluster.high_buckets)
+        self.symbols += cluster.symbols
+        self.cap_range_min = min(self.cap_range_min, cluster.cap_range_min)
+        self.cap_range_max = max(self.cap_range_max, cluster.cap_range_max)
+        self.sectors += cluster.sectors
+        self.industries += cluster.industries
+    def graph(self):
+    """graph deltas between buckets"""
+    def __str__(self):
+    """print all cluster data"""
         
 for stock in stocks:
-    groups = find_groups(stock)
+    groups = find_groups(stock, threshold)
     for group in groups:
         cluster = Cluster(group, stock)
         all_clusters.append(cluster)
         
-out = open('clusters.txt', 'wb')
+out = open(str(threshold) + '_size_clusters.txt', 'wb')
 pickle.dump(all_clusters, out)
