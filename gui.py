badfbd8bc0ef7c1ed7470bd004fb3c096162303b
michaelrauh <michaelrauh@ymail.com>
2015-06-08 20:00:24 -0400

    Made the program cache stock data locally so that subsequent runs are faster.

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
 import Tkinter as Tk
-import numpy as np
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
--- scraper.py
+++ scraper.py
@@ -1,4 +1,7 @@
 import urllib2
+import os
+import datetime
+import cPickle as pickle
 
 
 def get_todays_symbols():
@@ -8,18 +11,28 @@ def get_todays_symbols():
 
 
 def get_data(symbol, ipo):
+    date = str(datetime.date.today())
+
+    try:
+        points = pickle.load(open("data/pickles/" + symbol + "/" + date + ".p", "rb"))
+    except:
         """Get Data from Google Finance"""
         if ipo == 'n/a':
             ipo = '1972'
 
         try:
-            PRESENT = '2015'
+            present = str(datetime.date.today().year)
             url = 'http://www.google.com/finance/historical?q=' + symbol + \
                   '&histperiod=daily&startdate=Jan+1%2C+' + ipo + \
-                  '&enddate=Dec+31%2C+' + PRESENT + '&output=csv'
+                  '&enddate=Dec+31%2C+' + present + '&output=csv'
             f = urllib2.urlopen(url).read()
             points = f.replace('\n', ',').split(',')
-        # TODO: catch specific error
         except:
             points = []
-        return points
+        try:
+            os.makedirs("data/pickles/" + symbol + "/")
+        except:
+            pass
+        f = open("data/pickles/" + symbol + "/" + date + ".p", 'wb')
+        pickle.dump(points, f)
+    return points
