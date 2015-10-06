e6ab738d5dbaebe7b21279a55eb740246fcd77cf
michaelrauh <michaelrauh@ymail.com>
2015-10-05 20:54:57 -0400

    Caching implemented with data warehouse module. closes #59

---
+++ data_warehouse.py
@@ -0,0 +1,24 @@
+import scraper
+import datetime
+import pickle
+import os
+
+
+def hash_arguments(symbol, start_date, end_date):
+    end_year = str(end_date.year)
+    end_month = str(end_date.month - 1)
+    end_day = str(end_date.day)
+    start_year = str(start_date.year)
+    start_month = str(start_date.month - 1)
+    start_day = str(start_date.day)
+    return 'data/' + symbol + end_year + end_month + end_day + start_year + start_month + start_day + '.p'
+
+
+def fetch(symbol, start_date=datetime.date(1950, 1, 1), end_date=datetime.date.today()):
+    path = hash_arguments(symbol, start_date, end_date)
+    if not os.path.exists(path):
+        data = scraper.scrape(symbol, start_date=datetime.date(1950, 1, 1), end_date=datetime.date.today())
+        pickle.dump(data, open(path, 'wb'))
+    else:
+        data = pickle.load(open(path, 'rb'))
+    return data
--- scraper.py
+++ scraper.py
@@ -2,13 +2,13 @@ import urllib.request
 import datetime
 
 
-def scrape(symbol, start_time=datetime.date(1950, 1, 1), end_time=datetime.date.today()):
-    end_year = str(end_time.year)
-    end_month = str(end_time.month - 1)
-    end_day = str(end_time.day)
-    start_year = str(start_time.year)
-    start_month = str(start_time.month - 1)
-    start_day = str(start_time.day)
+def scrape(symbol, start_date=datetime.date(1950, 1, 1), end_date=datetime.date.today()):
+    end_year = str(end_date.year)
+    end_month = str(end_date.month - 1)
+    end_day = str(end_date.day)
+    start_year = str(start_date.year)
+    start_month = str(start_date.month - 1)
+    start_day = str(start_date.day)
     
     url = 'http://ichart.finance.yahoo.com/table.csv?s=' + symbol + '&d=' + end_month + '&e=' + end_day + '&f=' + \
           end_year + '&g=d&a=' + start_month + '&b=' + start_day + '&c=' + start_year + '&ignore=.csv'
