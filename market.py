2ff4605e3772a366e11e77d3607433a77f091209
michaelrauh <michaelrauh@ymail.com>
2015-10-09 17:00:27 -0400

    updated market and stock to be able to fetch stocks with or without preloading. Specifying a date range is optional. So far, there is no custom functionality on Stock or Market

--- market.py
+++ market.py
@@ -1,8 +1,27 @@
 import stock
-import data_warehouse
-import static_data
+import static_data as data
+import datetime
+
 
 class Market:
     def __init__(self):
-        static = static_data.Static_data()
-        self.symbols = static.symbols()
+        self.symbols = data.symbols
+        self.stocks = {}
+
+    def load(self, symbol, start_date=datetime.date(1950, 1, 1), end_date=datetime.date.today()):
+        cap = data.cap(symbol)
+        ipo = data.ipo(symbol)
+        sector = data.sector(symbol)
+        industry = data.industry(symbol)
+        self.stocks[symbol] = stock.Stock(symbol, cap, ipo, sector, industry, start_date, end_date)
+
+    def load_all(self, start_date=datetime.date(1950, 1, 1), end_date=datetime.date.today()):
+        for symbol in self.symbols:
+            self.load(symbol, start_date, end_date)
+
+    def fetch(self, symbol, start_date=datetime.date(1950, 1, 1), end_date=datetime.date.today()):
+        try:
+            return self.stocks[symbol]
+        except KeyError:
+            self.load(symbol, start_date, end_date)
+            return self.stocks[symbol]
--- stock.py
+++ stock.py
@@ -4,5 +4,9 @@ import datetime
 
 class Stock:
 
-    def __init__(self, symbol, start_date=datetime.date(1950, 1, 1), end_date=datetime.date.today()):
+    def __init__(self, symbol, cap, ipo, sector, industry, start_date=datetime.date(1950, 1, 1), end_date=datetime.date.today()):
         self.data = data_warehouse.fetch(symbol, start_date, end_date)
+        self.cap = cap
+        self.ipo = ipo
+        self.sector = sector
+        self.industry = industry
