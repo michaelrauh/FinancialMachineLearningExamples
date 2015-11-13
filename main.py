e0f7e1bbe353a7036738d718b968012488922e82
michaelrauh <michaelrauh@ymail.com>
2015-11-12 19:12:39 -0500

    added output to a file. Fixes #83.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import trader as t
 import datetime as d
 import matplotlib.pyplot as plt
+import os
 
 START = d.date(2005, 1, 1)
 END = d.date(2015, 10, 30)
@@ -13,5 +14,7 @@ x = 1
 trader.reset()
 trader.top_x(x, START, END, HORIZON, balances)
 plt.plot(balances)
+os.makedirs('output', exist_ok=True)
+plt.savefig('output/' + str(START) + str(END) + str(HORIZON) + str(x) + 'output.png', bbox_inches='tight')
 plt.show()
 print("Multiplier when x =", x, "is", (trader.balance() - 1000000)/1000000)
