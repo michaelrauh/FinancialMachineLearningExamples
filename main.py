5951726209b7a7602892e09af3f42e4481bf8281
michaelrauh <michaelrauh@ymail.com>
2015-11-15 09:44:01 -0500

    fixed graphical output. It was smashing all of the graphs together before.

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ START_ERA = d.date(2005, 1, 1)
 END_ERA = d.date(2015, 10, 30)
 START_SIM = d.date(2005, 1, 1)
 END_SIM = d.date(2015, 10, 30)
-HORIZON = 365
 
 trader = t.Trader(1000000, START_ERA, END_ERA)
-balances = []
 
-x = 1
-trader.reset()
-trader.top_x(x, START_SIM, END_SIM, HORIZON, balances)
-plt.plot(balances)
-os.makedirs('output', exist_ok=True)
-plt.savefig('output/' + str(START_SIM) + str(END_SIM) + str(HORIZON) + str(x) + 'output.png', bbox_inches='tight')
-plt.show()
-print("Multiplier when x =", x, "is", (trader.balance() - 1000000)/1000000)
+for x in [1,2,3,5,8,13,21,35,80,150,300,600]:
+    for horizon in [1,2,3,5,8,13,21,35,80,150,300,600]:
+        balances = []
+        trader.reset()
+        trader.top_x(x, START_SIM, END_SIM, horizon, balances)
+        plt.plot(balances)
+        os.makedirs('output', exist_ok=True)
+        plt.savefig('output/' + str(START_SIM) + "_" + str(END_SIM) + "_" + str(x) + "_" + str(horizon) + '.png', bbox_inches='tight')
+        # plt.show()
+        plt.gcf().clear()
+        print("x=", x, "horizon=", horizon, "balance=", (trader.balance()))
