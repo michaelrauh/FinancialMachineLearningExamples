424d50bd9567745ac7a8e2ee2f7346b16d35bcd6
michaelrauh <michaelrauh@ymail.com>
2015-12-01 18:19:48 -0500

    making code pep8 compliant again.

--- grapher.py
+++ grapher.py
@@ -4,7 +4,8 @@ import os
 
 def graph(balances, balance, horizon, x, start_sim, end_sim, loss, blacklist_duration):
     plt.plot(balances)
-    plt.title("x=" + str(x) + ", horizon=" + str(horizon) + ", balance=" + str(round(balance, 2)) + " stop at " + str(loss) + "duration " + str(blacklist_duration))
+    plt.title("x=" + str(x) + ", horizon=" + str(horizon) + ", balance=" + str(round(balance, 2)) + " stop at " +
+              str(loss) + "duration " + str(blacklist_duration))
     plt.xlabel("number of days after Jan. 1, 2005")
     plt.ylabel("Portfolio value (USD)")
     os.makedirs('output', exist_ok=True)
