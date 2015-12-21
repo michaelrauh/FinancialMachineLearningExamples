0428d32ffb2a32564e9b512fa2b8dba608539b64
michaelrauh <michaelrauh@ymail.com>
2015-12-20 23:53:16 -0500

    added time tracking for runtimes. Last commit seemed to be dropping triggers. It looks like Python doesn't like it when there is a very large (>100) number of traders.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import trader as t
 import datetime as d
 import grapher as g
 import market as m
+import time
+
+start_time = time.time()
 
 START_ERA = d.date(2005, 1, 1)
 END_ERA = d.date(2015, 10, 30)
@@ -12,11 +15,14 @@ market = m.Market(START_ERA, END_ERA)
 current_date = START_SIM
 traders = []
 
-for portfolio_size in [1, 2, 3, 5, 8, 13, 21, 34]:
-    for loss in [-.01, -.05, -.1, -1]:
-        for blacklist_duration in [30, 90]:
+for portfolio_size in [2, 3, 5, 8]:
+    for loss in [-.1, -.05]:
+        for blacklist_duration in [30]:
             for strategy in ["vanilla", "stop_loss"]:
                 traders.append(t.Trader(10000, market, strategy, portfolio_size, 365, loss, blacklist_duration))
+
+for trader in traders:
+    print(trader.portfolio_size, trader.loss, trader.blacklist_duration, trader.strategy)
 
 balances = [[] for i in range(len(traders))]
 
@@ -30,3 +36,7 @@ while current_date < END_SIM - d.timedelta(30):
 for trader, i in zip(traders, range(len(traders))):
     g.graph(balances[i], trader.portfolio.value() + trader.account.balance, trader.horizon, trader.portfolio_size,
             START_SIM, END_SIM, trader.loss, trader.blacklist_duration, i, trader.strategy)
+
+end_time = time.time()
+
+print("total time elapsed:", (end_time - start_time)/60, "minutes")
